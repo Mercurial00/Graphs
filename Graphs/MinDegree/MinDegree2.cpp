3585ef51aab228eaf538e70ef8652a4b7936cd4b
Mercurial00 <148081131+Mercurial00@users.noreply.github.com>
2025-12-05 19:43:43 +0300

    MinDegree algotithm v3.0
    Improved search and processing of indistinguishable vertices. Significant acceleration of execution

--- Graphs/MinDegree/MinDegree2.cpp
+++ Graphs/MinDegree/MinDegree2.cpp
@@ -3,11 +3,12 @@
 std::vector<int> adj(const int& x, const std::vector<std::vector<int>>& NODES, std::vector<int> mask) {
 	using namespace std;
 	vector<int> adj_;
+	mask[x] = 2;
 	//adj_.reserve(NODES.size());
-	for (auto i : NODES[x]) {
-		if (mask[i] == -1 || mask[i] == 1) {
-			for (auto j : NODES[i]) {
-				if (mask[j] == 0 && j != x) {
+	for (const auto& i : NODES[x]) {
+		if (mask[i] == -1) {
+			for (const auto& j : NODES[i]) {
+				if (mask[j] == 0) {
 					mask[j] = 2;
 					adj_.push_back(j);
 				}
@@ -21,36 +22,12 @@ std::vector<int> adj(const int& x, const std::vector<std::vector<int>>& NODES, s
 	return adj_;
 }
 
-//void transform_(const int& x, std::vector<std::vector<int>>& NODES, std::vector<int>& mask) {
-//	using namespace std;
-//	vector<int> adj_ = adj(x, NODES, mask);
-//	for (auto elem : NODES[x]) {
-//		if (mask[elem] == -1) {
-//			mask[elem] = 1;
-//		}
-//	}
-//	NODES[x] = adj_;
-//	mask[x] = -1;
-//	for (auto y : NODES[x]) {
-//		bool flag = false;
-//		for (int i = 0; i < NODES[y].size(); ++i) {
-//			if (NODES[y][i] == x) flag = true;
-//			if (mask[NODES[y][i]] == 1) {
-//				NODES[y].erase(NODES[y].begin() + i);
-//			}
-//		}
-//		if (!flag) {
-//			NODES[y].push_back(x);
-//		}
-//	}
-//}
-
 int degree(const int& x, const std::vector<std::vector<int>>& NODES, std::vector<int> mask) {
 	int deg = 0;
 	mask[x] = 2;
-	for (auto i : NODES[x]) {
+	for (const auto& i : NODES[x]) {
 		if (mask[i] == -1) {
-			for (auto j : NODES[i]) {
+			for (const auto& j : NODES[i]) {
 				if (mask[j] == 0) {
 					++deg;
 					mask[j] = 2;
@@ -67,42 +44,45 @@ int degree(const int& x, const std::vector<std::vector<int>>& NODES, std::vector
 
 void transform_(std::queue<int>& x, std::vector<std::vector<int>>& NODES, std::vector<int>& mask, int* perm, int& num) {
 	using namespace std;
-	for (auto elem : NODES[x.back()]) {
-		if (mask[elem] == -1) {
-			mask[elem] = 1;
+	for (const auto& y : NODES[x.back()]) {
+		if (mask[y] == -1) {
+			mask[y] = 1;
 		}
 	}
 	if (x.size() != 1) {
-		int t = NODES[x.front()].size();
-		int front = x.front();
+		//int t = NODES[x.front()].size();
+		//int front = x.front();
+		NODES[x.back()].swap(NODES[x.front()]);
 		while (x.size() > 1) {
 			mask[x.front()] = 1;
 			perm[x.front()] = num++;
 			x.pop();
 		}
-		NODES[x.back()].resize(0);
-		for (int i = 0; i < t; ++i) {
-			if (NODES[front][i] != x.back() && mask[NODES[front][i]] != 1) {
-				NODES[x.back()].push_back(NODES[front][i]);
-			}
-		}
+		//NODES[x.back()].resize(0);
+		//for (int i = 0; i < t; ++i) {
+		//	if (mask[NODES[front][i]] != 1 && NODES[front][i] != x.back()) {
+		//		NODES[x.back()].push_back(NODES[front][i]);
+		//	}
+		//}
 	}
-	//vector<int> adj_ = adj(x.back(), NODES, mask);
-	//NODES[x.back()] = adj(x.back(), NODES, mask);
 	mask[x.front()] = -1;
 	perm[x.front()] = num++;
-	for (auto y : NODES[x.back()]) {
+	//?
+	for (const auto& y : NODES[x.front()]) {
+		if (mask[y] != 0) {
+			continue;
+		}
 		bool flag = false;
 		vector<int> tmp;
 		for (int i = 0; i < NODES[y].size(); ++i) {
-			if (NODES[y][i] == x.back()) flag = true;
 			if (mask[NODES[y][i]] != 1) {
+				if (!flag && NODES[y][i] == x.front()) flag = true;
 				tmp.push_back(NODES[y][i]);
 			}
 		}
 		if (!flag) {
+			tmp.push_back(x.front());
 			//NODES[y].push_back(x.back());
-			tmp.push_back(x.back());
 		}
 		NODES[y].swap(tmp);
 	}
@@ -114,7 +94,6 @@ void MinDegree(const int& n, int* Rst, int* Col, int* perm) {
 	vector<int> degrees(n);
 	vector<int> mask(n);
 	vector<vector<int>> NODES(n);
-	vector<int*> QLINK;
 	for (int i = 0; i < n; ++i) {
 		for (int j = Rst[i]; j < Rst[i + 1]; ++j) {
 			NODES[i].push_back(Col[j]);
@@ -136,11 +115,8 @@ void MinDegree(const int& n, int* Rst, int* Col, int* perm) {
 		indis.push(x);
 		NODES[x] = adj(x, NODES, mask);
 		sort(NODES[x].begin(), NODES[x].end());
-		for (int y = 0; y < n; ++y) {
-			if (y == x || mask[y] != 0) {
-				continue;
-			}
-			else if (degrees[x] == degrees[y]) {
+		for (const auto& y : NODES[x]) {
+			if (mask[y] == 0 && degrees[x] == degrees[y]) {
 				bool indistinguishable = true;
 				vector<int> y_reach = adj(y, NODES, mask);
 				sort(y_reach.begin(), y_reach.end());
@@ -154,7 +130,7 @@ void MinDegree(const int& n, int* Rst, int* Col, int* perm) {
 						++j;
 						continue;
 					}
-					if (NODES[x][i++] != y_reach[j++]) {
+					else if (NODES[x][i++] != y_reach[j++]) {
 						indistinguishable = false;
 						break;
 					}
@@ -164,10 +140,11 @@ void MinDegree(const int& n, int* Rst, int* Col, int* perm) {
 				}
 			}
 		}
-		//perm[x] = num++;
 		transform_(indis, NODES, mask, perm, num);
-		for (int i = 0; i < NODES[indis.back()].size(); ++i) {
-			degrees[NODES[indis.back()][i]] = degree(NODES[indis.back()][i], NODES, mask);
+		for (int i = 0; i < NODES[indis.front()].size(); ++i) {
+			if (mask[NODES[indis.front()][i]] == 0) {
+				degrees[NODES[indis.front()][i]] = degree(NODES[indis.front()][i], NODES, mask);
+			}
 		}
 	}
 }
